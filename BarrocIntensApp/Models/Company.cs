9f3625990c3a283923b600f079980e69272a3272
miitchel <mitchelvangils@gmail.com>
2022-10-11 16:06:58 +0200

    properties voor classes gemaakt

--- BarrocIntensApp/Models/Company.cs
+++ BarrocIntensApp/Models/Company.cs
@@ -8,5 +8,14 @@ namespace BarrocIntensApp.Models
 {
     internal class Company
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string City { get; set; }
+        public string CountryCode { get; set; }
+        public DateTime BkrCheckedAt { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhoneNumber { get; set; }
     }
 }
