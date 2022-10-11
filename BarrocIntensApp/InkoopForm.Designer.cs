cc3af4f9bc6c66f794613c7ddeaab30d3500be57
miitchel <mitchelvangils@gmail.com>
2022-10-11 11:12:02 +0200

    Zwarte rechthoek bovenaan met label

--- BarrocIntensApp/InkoopForm.Designer.cs
+++ BarrocIntensApp/InkoopForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace BarrocIntensApp
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace BarrocIntensApp
 {
     partial class InkoopForm
     {
@@ -29,11 +32,14 @@
         private void InitializeComponent()
         {
             this.btnMagazijn = new System.Windows.Forms.Button();
+            this.pbBlack = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).BeginInit();
             this.SuspendLayout();
             // 
             // btnMagazijn
             // 
-            this.btnMagazijn.Location = new System.Drawing.Point(12, 74);
+            this.btnMagazijn.Location = new System.Drawing.Point(12, 63);
             this.btnMagazijn.Name = "btnMagazijn";
             this.btnMagazijn.Size = new System.Drawing.Size(177, 170);
             this.btnMagazijn.TabIndex = 0;
@@ -41,20 +47,48 @@
             this.btnMagazijn.UseVisualStyleBackColor = true;
             this.btnMagazijn.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pbBlack
+            // 
+            this.pbBlack.BackColor = System.Drawing.SystemColors.WindowText;
+            this.pbBlack.Location = new System.Drawing.Point(-2, -1);
+            this.pbBlack.Name = "pbBlack";
+            this.pbBlack.Size = new System.Drawing.Size(802, 43);
+            this.pbBlack.TabIndex = 1;
+            this.pbBlack.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(639, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inkoop | Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // InkoopForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbBlack);
             this.Controls.Add(this.btnMagazijn);
             this.Name = "InkoopForm";
             this.Text = "Inkoop";
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnMagazijn;
+        private PictureBox pbBlack;
+        private Label label1;
     }
 }
