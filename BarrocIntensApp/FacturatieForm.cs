3ba919a89eee84252307799a359440cb90bed187
adam110311 <70455243+adam110311@users.noreply.github.com>
2022-10-11 11:42:25 +0200

    Added finance styling

--- BarrocIntensApp/FacturatieForm.cs
+++ BarrocIntensApp/FacturatieForm.cs
@@ -16,5 +16,10 @@ namespace BarrocIntensApp
         {
             InitializeComponent();
         }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- BarrocIntensApp/FinanceForm.Designer.cs
+++ BarrocIntensApp/FinanceForm.Designer.cs
@@ -31,6 +31,9 @@
             this.FacturatieBtn = new System.Windows.Forms.Button();
             this.LeaseBtn = new System.Windows.Forms.Button();
             this.KostenBtn = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pbBlack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).BeginInit();
             this.SuspendLayout();
             // 
             // FacturatieBtn
@@ -63,17 +66,45 @@
             this.KostenBtn.UseVisualStyleBackColor = true;
             this.KostenBtn.Click += new System.EventHandler(this.KostenBtn_Click);
             // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Black;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(875, 9);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(190, 31);
+            this.lblTitle.TabIndex = 8;
+            this.lblTitle.Text = "Finance | Main";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pbBlack
+            // 
+            this.pbBlack.BackColor = System.Drawing.SystemColors.WindowText;
+            this.pbBlack.Location = new System.Drawing.Point(-385, 0);
+            this.pbBlack.Margin = new System.Windows.Forms.Padding(4);
+            this.pbBlack.Name = "pbBlack";
+            this.pbBlack.Size = new System.Drawing.Size(1527, 53);
+            this.pbBlack.TabIndex = 7;
+            this.pbBlack.TabStop = false;
+            // 
             // FinanceForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1142, 593);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pbBlack);
             this.Controls.Add(this.KostenBtn);
             this.Controls.Add(this.LeaseBtn);
             this.Controls.Add(this.FacturatieBtn);
             this.Name = "FinanceForm";
             this.Text = "Finance_mainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,5 +113,7 @@
         private System.Windows.Forms.Button FacturatieBtn;
         private System.Windows.Forms.Button LeaseBtn;
         private System.Windows.Forms.Button KostenBtn;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pbBlack;
     }
 }
