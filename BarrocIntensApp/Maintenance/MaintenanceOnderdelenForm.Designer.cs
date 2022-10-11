745060ddd77340ff8509aad998708ce06f5bc3d6
guy van wonderen <72141286+guyyves@users.noreply.github.com>
2022-10-11 16:35:22 +0200

    Pijl

--- BarrocIntensApp/Maintenance/MaintenanceOnderdelenForm.Designer.cs
+++ BarrocIntensApp/Maintenance/MaintenanceOnderdelenForm.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MaintenanceOnderdelenForm));
             this.lblTitle = new System.Windows.Forms.Label();
             this.pbBlack = new System.Windows.Forms.PictureBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.BtnReturnStoringen = new BarrocIntensApp.RoundButton();
             ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).BeginInit();
             this.SuspendLayout();
@@ -57,15 +58,23 @@
             this.pbBlack.TabIndex = 5;
             this.pbBlack.TabStop = false;
             // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 1);
+            this.monthCalendar1.Location = new System.Drawing.Point(402, 176);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 9;
+            // 
             // BtnReturnStoringen
             // 
             this.BtnReturnStoringen.BackColor = System.Drawing.Color.Black;
+            this.BtnReturnStoringen.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnReturnStoringen.ForeColor = System.Drawing.Color.White;
             this.BtnReturnStoringen.Location = new System.Drawing.Point(-2, 47);
             this.BtnReturnStoringen.Name = "BtnReturnStoringen";
-            this.BtnReturnStoringen.Size = new System.Drawing.Size(124, 37);
+            this.BtnReturnStoringen.Size = new System.Drawing.Size(88, 50);
             this.BtnReturnStoringen.TabIndex = 8;
-            this.BtnReturnStoringen.Text = "Terug";
+            this.BtnReturnStoringen.Text = "←";
             this.BtnReturnStoringen.UseVisualStyleBackColor = false;
             this.BtnReturnStoringen.Click += new System.EventHandler(this.BtnReturnStoringen_Click);
             // 
@@ -74,6 +83,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1144, 601);
+            this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.BtnReturnStoringen);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.pbBlack);
@@ -91,5 +101,6 @@
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.PictureBox pbBlack;
         private RoundButton BtnReturnStoringen;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
     }
 }
