8cfb35fb2e63040628f2431f7014d97bdafd18d3
NickSchoenmakers <71122447+NickSchoenmakers@users.noreply.github.com>
2022-10-11 10:06:36 +0200

    changed title of home screen

--- BarrocIntensApp/Program.cs
+++ BarrocIntensApp/Program.cs
@@ -16,7 +16,7 @@ namespace BarrocIntensApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
