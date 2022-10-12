0c33a51ed09043737ed9d0ba577eb23f3df82d2d
adam110311 <70455243+adam110311@users.noreply.github.com>
2022-10-12 13:57:38 +0200

    factuur testing
    testen van basis factuur functionaliteiten

--- BarrocIntensApp/Finance/FinanceFacturatieForm.Designer.cs
+++ BarrocIntensApp/Finance/FinanceFacturatieForm.Designer.cs
@@ -28,27 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FacturatieForm));
             this.lblTitle = new System.Windows.Forms.Label();
             this.pbBlack = new System.Windows.Forms.PictureBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ProductCbx = new System.Windows.Forms.ComboBox();
+            this.NameTb = new System.Windows.Forms.TextBox();
+            this.StreetTb = new System.Windows.Forms.TextBox();
+            this.ZipTb = new System.Windows.Forms.TextBox();
             this.NameLbl = new System.Windows.Forms.Label();
             this.Street_numberLbl = new System.Windows.Forms.Label();
             this.ZipcodeLbl = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.HnrTb = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.CityTb = new System.Windows.Forms.TextBox();
+            this.MonthlyRb = new System.Windows.Forms.RadioButton();
+            this.PeriodRb = new System.Windows.Forms.RadioButton();
             this.label3 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.AmountNu = new System.Windows.Forms.NumericUpDown();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.BtnReturnStoringen = new BarrocIntensApp.RoundButton();
             ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmountNu)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -73,34 +76,38 @@
             this.pbBlack.TabIndex = 5;
             this.pbBlack.TabStop = false;
             // 
-            // comboBox1
+            // ProductCbx
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(110, 245);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 7;
+            this.ProductCbx.FormattingEnabled = true;
+            this.ProductCbx.Location = new System.Drawing.Point(131, 222);
+            this.ProductCbx.Name = "ProductCbx";
+            this.ProductCbx.Size = new System.Drawing.Size(121, 21);
+            this.ProductCbx.TabIndex = 7;
+            this.ProductCbx.SelectedIndexChanged += new System.EventHandler(this.ProductCbx_SelectedIndexChanged);
             // 
-            // textBox1
+            // NameTb
             // 
-            this.textBox1.Location = new System.Drawing.Point(131, 92);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 8;
+            this.NameTb.Location = new System.Drawing.Point(131, 92);
+            this.NameTb.Name = "NameTb";
+            this.NameTb.Size = new System.Drawing.Size(173, 20);
+            this.NameTb.TabIndex = 8;
+            this.NameTb.TextChanged += new System.EventHandler(this.NameTb_TextChanged);
             // 
-            // textBox2
+            // StreetTb
             // 
-            this.textBox2.Location = new System.Drawing.Point(131, 118);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 9;
+            this.StreetTb.Location = new System.Drawing.Point(131, 118);
+            this.StreetTb.Name = "StreetTb";
+            this.StreetTb.Size = new System.Drawing.Size(173, 20);
+            this.StreetTb.TabIndex = 9;
+            this.StreetTb.TextChanged += new System.EventHandler(this.StreetTb_TextChanged);
             // 
-            // textBox3
+            // ZipTb
             // 
-            this.textBox3.Location = new System.Drawing.Point(131, 196);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 10;
+            this.ZipTb.Location = new System.Drawing.Point(131, 196);
+            this.ZipTb.Name = "ZipTb";
+            this.ZipTb.Size = new System.Drawing.Size(173, 20);
+            this.ZipTb.TabIndex = 10;
+            this.ZipTb.TextChanged += new System.EventHandler(this.ZipTb_TextChanged);
             // 
             // NameLbl
             // 
@@ -133,18 +140,19 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(60, 248);
+            this.label8.Location = new System.Drawing.Point(81, 225);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(44, 13);
             this.label8.TabIndex = 22;
             this.label8.Text = "Product";
             // 
-            // textBox8
+            // HnrTb
             // 
-            this.textBox8.Location = new System.Drawing.Point(131, 144);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
-            this.textBox8.TabIndex = 23;
+            this.HnrTb.Location = new System.Drawing.Point(131, 144);
+            this.HnrTb.Name = "HnrTb";
+            this.HnrTb.Size = new System.Drawing.Size(173, 20);
+            this.HnrTb.TabIndex = 23;
+            this.HnrTb.TextChanged += new System.EventHandler(this.HnrTb_TextChanged);
             // 
             // label1
             // 
@@ -164,87 +172,118 @@
             this.label2.TabIndex = 26;
             this.label2.Text = "Stad";
             // 
-            // textBox9
+            // CityTb
             // 
-            this.textBox9.Location = new System.Drawing.Point(131, 170);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 20);
-            this.textBox9.TabIndex = 25;
+            this.CityTb.Location = new System.Drawing.Point(131, 170);
+            this.CityTb.Name = "CityTb";
+            this.CityTb.Size = new System.Drawing.Size(173, 20);
+            this.CityTb.TabIndex = 25;
+            this.CityTb.TextChanged += new System.EventHandler(this.CityTb_TextChanged);
             // 
-            // radioButton1
+            // MonthlyRb
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(71, 222);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(81, 17);
-            this.radioButton1.TabIndex = 27;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Maandelijks";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.MonthlyRb.AutoSize = true;
+            this.MonthlyRb.Location = new System.Drawing.Point(133, 249);
+            this.MonthlyRb.Name = "MonthlyRb";
+            this.MonthlyRb.Size = new System.Drawing.Size(81, 17);
+            this.MonthlyRb.TabIndex = 27;
+            this.MonthlyRb.TabStop = true;
+            this.MonthlyRb.Text = "Maandelijks";
+            this.MonthlyRb.UseVisualStyleBackColor = true;
+            this.MonthlyRb.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // radioButton2
+            // PeriodRb
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(162, 222);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(69, 17);
-            this.radioButton2.TabIndex = 28;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Periodiek";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.PeriodRb.AutoSize = true;
+            this.PeriodRb.Location = new System.Drawing.Point(235, 249);
+            this.PeriodRb.Name = "PeriodRb";
+            this.PeriodRb.Size = new System.Drawing.Size(69, 17);
+            this.PeriodRb.TabIndex = 28;
+            this.PeriodRb.TabStop = true;
+            this.PeriodRb.Text = "Periodiek";
+            this.PeriodRb.UseVisualStyleBackColor = true;
+            this.PeriodRb.CheckedChanged += new System.EventHandler(this.PeriodRb_CheckedChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(237, 248);
+            this.label3.Location = new System.Drawing.Point(258, 225);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 13);
             this.label3.TabIndex = 29;
             this.label3.Text = "Hoeveelheid";
             // 
-            // numericUpDown1
+            // AmountNu
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(310, 245);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.AmountNu.Location = new System.Drawing.Point(331, 222);
+            this.AmountNu.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.AmountNu.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
-            this.numericUpDown1.TabIndex = 30;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.AmountNu.Name = "AmountNu";
+            this.AmountNu.Size = new System.Drawing.Size(50, 20);
+            this.AmountNu.TabIndex = 30;
+            this.AmountNu.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
+            this.AmountNu.ValueChanged += new System.EventHandler(this.AmountNu_ValueChanged);
+            // 
+            // listView1
+            // 
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "listViewGroup1";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(741, 49);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(391, 427);
+            this.listView1.TabIndex = 31;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // BtnReturnStoringen
+            // 
+            this.BtnReturnStoringen.BackColor = System.Drawing.Color.Black;
+            this.BtnReturnStoringen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReturnStoringen.ForeColor = System.Drawing.Color.White;
+            this.BtnReturnStoringen.Location = new System.Drawing.Point(1044, 539);
+            this.BtnReturnStoringen.Name = "BtnReturnStoringen";
+            this.BtnReturnStoringen.Size = new System.Drawing.Size(88, 50);
+            this.BtnReturnStoringen.TabIndex = 32;
+            this.BtnReturnStoringen.Text = "Bestel";
+            this.BtnReturnStoringen.UseVisualStyleBackColor = false;
             // 
             // FacturatieForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1144, 601);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.BtnReturnStoringen);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.AmountNu);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.PeriodRb);
+            this.Controls.Add(this.MonthlyRb);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.CityTb);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.HnrTb);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.ZipcodeLbl);
             this.Controls.Add(this.Street_numberLbl);
             this.Controls.Add(this.NameLbl);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ZipTb);
+            this.Controls.Add(this.StreetTb);
+            this.Controls.Add(this.NameTb);
+            this.Controls.Add(this.ProductCbx);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.pbBlack);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -253,7 +292,7 @@
             this.Text = "Facturatie_Form";
             this.Load += new System.EventHandler(this.FacturatieForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AmountNu)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -263,21 +302,23 @@
 
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.PictureBox pbBlack;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox ProductCbx;
+        private System.Windows.Forms.TextBox NameTb;
+        private System.Windows.Forms.TextBox StreetTb;
+        private System.Windows.Forms.TextBox ZipTb;
         private System.Windows.Forms.Label NameLbl;
         private System.Windows.Forms.Label Street_numberLbl;
         private System.Windows.Forms.Label ZipcodeLbl;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox HnrTb;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox CityTb;
+        private System.Windows.Forms.RadioButton MonthlyRb;
+        private System.Windows.Forms.RadioButton PeriodRb;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown AmountNu;
+        private System.Windows.Forms.ListView listView1;
+        private RoundButton BtnReturnStoringen;
     }
 }
