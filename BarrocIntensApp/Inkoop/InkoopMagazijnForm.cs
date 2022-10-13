1aef71eb71beb68f498618a8259ab51c768f2df7
miitchel <mitchelvangils@gmail.com>
2022-10-13 16:49:46 +0200

    product beheren klaar

--- BarrocIntensApp/Inkoop/InkoopMagazijnForm.cs
+++ BarrocIntensApp/Inkoop/InkoopMagazijnForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BarrocIntensApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +21,45 @@ namespace BarrocIntensApp
 
         private void InkoopMagazijnForm_Load(object sender, EventArgs e)
         {
-            
+            Program.dbContext.Products.Load();
+            Program.dbContext.ProductCategories.Load();
+
+            this.productCategoriesGridView2.DataSource = Program.dbContext.ProductCategories.Local.ToBindingList();
+        }
+
+        private void productCategoriesGridView2_SelectionChanged(object sender, EventArgs e)
+        {
+            if (Program.dbContext == null)
+                return;
+
+            var productCategory = (ProductCategory)this.productCategoriesGridView2.CurrentRow?.DataBoundItem;
+
+            if (productCategory == null)
+                return;
+
+            productsDataGridView.DataSource = productCategory.Products;
+        }
+
+        private void productsDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            if (Program.dbContext == null)
+                return;
+
+            var product = (Product)this.productsDataGridView.CurrentRow?.DataBoundItem;
+
+            if (product == null)
+                return;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (Program.dbContext == null)
+                return;
+
+            Program.dbContext.SaveChanges();
+
+            this.productCategoriesGridView2.Refresh();
+            this.productsDataGridView.Refresh();
         }
     }
 }
