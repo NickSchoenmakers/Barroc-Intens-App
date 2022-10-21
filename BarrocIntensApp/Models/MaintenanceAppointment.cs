d93d704a93d812fbe89d9184cbf1b243ef2b89fc
miitchel <60847304+miitchel@users.noreply.github.com>
2022-10-21 19:57:29 +0200

    manager form voor inplannen toegevoegd, maintenance werknemer kan zijn afspraken zien, manager kan voor elke maintenance werknemer een afspraak inplannen

--- BarrocIntensApp/Models/MaintenanceAppointment.cs
+++ BarrocIntensApp/Models/MaintenanceAppointment.cs
@@ -15,6 +15,8 @@ namespace BarrocIntensApp.Models
         public DateTime? LastAppointment { get; set; } = null;
         public int CompanyId { get; set; }
         public Company Company { get; set; } = null;
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null;
         public int WorkerId { get; set; }
         public User Worker { get; set; } = null;
     }
