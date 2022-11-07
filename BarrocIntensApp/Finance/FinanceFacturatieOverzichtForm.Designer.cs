98bc5b89931891437510265b42e03d73f7922428
adam110311 <70455243+adam110311@users.noreply.github.com>
2022-11-07 10:10:23 +0100

    l

--- BarrocIntensApp/Finance/FinanceFacturatieOverzichtForm.Designer.cs
+++ BarrocIntensApp/Finance/FinanceFacturatieOverzichtForm.Designer.cs
@@ -28,12 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbBlack = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbBlack
+            // 
+            this.pbBlack.BackColor = System.Drawing.SystemColors.WindowText;
+            this.pbBlack.Location = new System.Drawing.Point(-282, -1);
+            this.pbBlack.Name = "pbBlack";
+            this.pbBlack.Size = new System.Drawing.Size(1434, 43);
+            this.pbBlack.TabIndex = 6;
+            this.pbBlack.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Black;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(954, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(178, 25);
+            this.lblTitle.TabIndex = 102;
+            this.lblTitle.Text = "Factuur overzicht";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // FinanceFacturatieOverzichtForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1144, 601);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pbBlack);
+            this.Name = "FinanceFacturatieOverzichtForm";
             this.Text = "FinanceFacturatieOverzichtForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbBlack;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
