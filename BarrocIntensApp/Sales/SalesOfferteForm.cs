0bdb722a17382dba496ba0a5d5b4a3d716404f01
Noor <noormga@gmail.com>
2022-11-07 11:49:14 +0100

    Made possible to generate a pdf file in sales offerte form

--- BarrocIntensApp/Sales/SalesOfferteForm.cs
+++ BarrocIntensApp/Sales/SalesOfferteForm.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -10,10 +14,17 @@ using System.Net;
 using System.Net.Mail;
 using System.Net.Mime;
 using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using static BarrocIntensApp.LoginForm;
+using static System.Net.Mime.MediaTypeNames;
+using Color = Aspose.Pdf.Color;
+using HorizontalAlignment = Aspose.Pdf.HorizontalAlignment;
+using Rectangle = Aspose.Pdf.Rectangle;
 
 namespace BarrocIntensApp
 {
@@ -24,6 +35,9 @@ namespace BarrocIntensApp
             InitializeComponent();
 
             lblTitle.Text = $"Sales | {Globals.loggedInUser.Name}";
+
+            Program.dbContext.Products.Load();
+            this.dgvProducts.DataSource = Program.dbContext.Products.Local.ToBindingList();
         }
 
         private void btnBackNotes_Click(object sender, EventArgs e)
@@ -33,34 +47,89 @@ namespace BarrocIntensApp
             sales.Show(this);
         }
 
-        private void BtnSendMail_Click(object sender, EventArgs e)
+        private void BtnSendMail_Click_1(object sender, EventArgs e)
         {
-            SendMailWithMailTo(
-                "D279851@edu.curio.nl",
-                "testing subject",
-                "testing body",
-                "C:/xampp/htdocs/Barroc-Intens-App/BarrocIntensApp/text.txt");
-            MessageBox.Show("email sent");
-        }
-        public static void SendMailWithMailTo(
-            string address,
-            string subject,
-            string body,
-            string attachment)
-        {
-            //Don't use this - just an example
-            string mailto =
-                string.Format(
-                    "mailto:{0}?Subject={1}&Body={2}&Attachment={3}",
-                    address, subject, body, attachment);
-            System.Diagnostics.Process.Start(mailto);
-        }
 
-        private void roundButton1_Click(object sender, EventArgs e)
-        {
-            var LoginForm = new LoginForm();
-            this.Hide();
-            LoginForm.Show(this);
+            this.Enabled = false;
+            this.pbLoad.Style = ProgressBarStyle.Marquee;
+            pbLoad.Visible = true;
+            pbLoad.BringToFront();
+
+            Task.Run(() =>
+            {
+                // Initialize document object
+                Document document = new Document();
+                // Add page
+                Page page = document.Pages.Add();
+                // Add image
+                var imageFileName = System.IO.Path.Combine("../../Image", "Logo6_groot.png");
+                page.AddImage(imageFileName, new Rectangle(20, 730, 120, 830));
+                // Add Header
+                var header = new TextFragment("Offerte");
+                header.TextState.Font = FontRepository.FindFont("Arial");
+                header.TextState.FontSize = 24;
+                header.HorizontalAlignment = HorizontalAlignment.Center;
+                header.Position = new Position(100, 700);
+                page.Paragraphs.Add(header);
+
+                //Aspose.Pdf.Rectangle rect = new Aspose.Pdf.Rectangle(100, 200, 300, 300);
+                //Aspose.Pdf.Forms.TextBoxField textBox = new Aspose.Pdf.Forms.TextBoxField(document.Pages[1], rect);
+                //textBox.Name = "textbox1";
+                //textBox.Multiline = true;
+                //textBox.Value = "jemoeder\nok";
+                //document.Form.Add(textBox, 1);
+
+                // Add description
+                var descriptionText = $"{txbFirstName.Text} {txbLastName.Text}";
+                var description = new TextFragment(descriptionText);
+                description.TextState.Font = FontRepository.FindFont("Times New Roman");
+                description.TextState.FontSize = 14;
+                description.Margin = new MarginInfo(0, 50, 0, 50);
+                page.Paragraphs.Add(description);
+
+
+                // Add table
+                var table = new Aspose.Pdf.Table
+                {
+                    ColumnWidths = "200",
+                    Border = new BorderInfo(BorderSide.Box, 1f, Color.DarkSlateGray),
+                    DefaultCellBorder = new BorderInfo(BorderSide.Box, 0.5f, Color.Black),
+                    DefaultCellPadding = new MarginInfo(4.5, 4.5, 4.5, 4.5),
+                    Margin =
+                {
+                    Bottom = 10
+                },
+                    DefaultCellTextState =
+                {
+                    Font =  FontRepository.FindFont("Helvetica")
+                }
+                };
+
+                var headerRow = table.Rows.Add();
+                headerRow.Cells.Add("Product");
+                headerRow.Cells.Add("Prijs");
+                foreach (Cell headerRowCell in headerRow.Cells)
+                {
+                    headerRowCell.BackgroundColor = Color.Gray;
+                    headerRowCell.DefaultCellTextState.ForegroundColor = Color.WhiteSmoke;
+                }
+
+                var dataRow = table.Rows.Add();
+                dataRow.Cells.Add("test1");
+                dataRow.Cells.Add("test2");
+
+                page.Paragraphs.Add(table);
+                // Save updated PDF
+                var outputFileName = System.IO.Path.Combine("Offerte.pdf");
+                document.Save(outputFileName);
+
+                Process.Start(outputFileName);
+
+                this.Invoke((MethodInvoker)delegate {
+                    this.Enabled = true;
+                    pbLoad.Visible = false;
+                });
+            });
         }
     }
 }
