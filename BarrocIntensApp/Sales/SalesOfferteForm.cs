32d8d13d8b1c0ff5ec82b6d40810fb1ef8896eab
Noor <noormga@gmail.com>
2022-11-08 16:22:15 +0100

    Finished the pdf that gets generated in sales offerte form

--- BarrocIntensApp/Sales/SalesOfferteForm.cs
+++ BarrocIntensApp/Sales/SalesOfferteForm.cs
@@ -1,5 +1,6 @@
 ﻿using Aspose.Pdf;
 using Aspose.Pdf.Text;
+using BarrocIntensApp.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,7 @@ using System.Windows.Forms;
 using System.Xml.Linq;
 using static BarrocIntensApp.LoginForm;
 using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using Color = Aspose.Pdf.Color;
 using HorizontalAlignment = Aspose.Pdf.HorizontalAlignment;
 using Rectangle = Aspose.Pdf.Rectangle;
@@ -30,9 +32,20 @@ namespace BarrocIntensApp
 {
     public partial class SalesOfferteForm : Form
     {
+        string fileLocation;
+        Product selectedProduct;
+        decimal total = 0;
+        List<decimal> prodAmounts = new List<decimal>();
+        int i = 0;
         public SalesOfferteForm()
         {
             InitializeComponent();
+
+            // C:\Users\Username:
+            string userRoot = System.Environment.GetEnvironmentVariable("USERPROFILE");
+            // C:\Users\Username\Downloads:
+            fileLocation = Path.Combine(userRoot, "Downloads");
+            lbFolder.Text = fileLocation;
 
             lblTitle.Text = $"Sales | {Globals.loggedInUser.Name}";
 
@@ -60,7 +73,7 @@ namespace BarrocIntensApp
                 // Initialize document object
                 Document document = new Document();
                 // Add page
-                Page page = document.Pages.Add();
+                Aspose.Pdf.Page page = document.Pages.Add();
                 // Add image
                 var imageFileName = System.IO.Path.Combine("../../Image", "Logo6_groot.png");
                 page.AddImage(imageFileName, new Rectangle(20, 730, 120, 830));
@@ -71,13 +84,6 @@ namespace BarrocIntensApp
                 header.HorizontalAlignment = HorizontalAlignment.Center;
                 header.Position = new Position(100, 700);
                 page.Paragraphs.Add(header);
-
-                //Aspose.Pdf.Rectangle rect = new Aspose.Pdf.Rectangle(100, 200, 300, 300);
-                //Aspose.Pdf.Forms.TextBoxField textBox = new Aspose.Pdf.Forms.TextBoxField(document.Pages[1], rect);
-                //textBox.Name = "textbox1";
-                //textBox.Multiline = true;
-                //textBox.Value = "jemoeder\nok";
-                //document.Form.Add(textBox, 1);
 
                 // Add description
                 var descriptionText = $"{txbFirstName.Text} {txbLastName.Text}";
@@ -91,7 +97,7 @@ namespace BarrocIntensApp
                 // Add table
                 var table = new Aspose.Pdf.Table
                 {
-                    ColumnWidths = "200",
+                    ColumnWidths = "150",
                     Border = new BorderInfo(BorderSide.Box, 1f, Color.DarkSlateGray),
                     DefaultCellBorder = new BorderInfo(BorderSide.Box, 0.5f, Color.Black),
                     DefaultCellPadding = new MarginInfo(4.5, 4.5, 4.5, 4.5),
@@ -106,6 +112,7 @@ namespace BarrocIntensApp
                 };
 
                 var headerRow = table.Rows.Add();
+                headerRow.Cells.Add("Aantal");
                 headerRow.Cells.Add("Product");
                 headerRow.Cells.Add("Prijs");
                 foreach (Cell headerRowCell in headerRow.Cells)
@@ -114,13 +121,26 @@ namespace BarrocIntensApp
                     headerRowCell.DefaultCellTextState.ForegroundColor = Color.WhiteSmoke;
                 }
 
-                var dataRow = table.Rows.Add();
-                dataRow.Cells.Add("test1");
-                dataRow.Cells.Add("test2");
+                foreach(var addedProduct in addedProducts)
+                {
+                    var price = Math.Round(addedProduct.Price, 2);
+                    var dataRow = table.Rows.Add();
+                    dataRow.Cells.Add(Convert.ToString(prodAmounts[i]));
+                    dataRow.Cells.Add(addedProduct.Name);
+                    dataRow.Cells.Add(" \u20AC" + Convert.ToString(price));
+                    price = price * prodAmounts[i];
+                    total += price;
+                    i++;
+                }
 
                 page.Paragraphs.Add(table);
+
+                var totalPrice = new TextFragment("Totaal:  \u20AC" + total);
+                totalPrice.HorizontalAlignment = HorizontalAlignment.Right;
+                page.Paragraphs.Add(totalPrice);
+
                 // Save updated PDF
-                var outputFileName = System.IO.Path.Combine("Offerte.pdf");
+                var outputFileName = System.IO.Path.Combine(fileLocation, "Offerte.pdf");
                 document.Save(outputFileName);
 
                 Process.Start(outputFileName);
@@ -131,6 +151,28 @@ namespace BarrocIntensApp
                 });
             });
         }
+
+        private void btnLocation_Click(object sender, EventArgs e)
+        {
+            DialogResult location = folderBrowserDialog1.ShowDialog();
+            if (location == DialogResult.OK)
+            {
+                fileLocation = folderBrowserDialog1.SelectedPath;
+                lbFolder.Text = fileLocation;
+            }
+        }
+        List<Product> addedProducts = new List<Product>();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            selectedProduct = (Product)this.dgvProducts.CurrentRow?.DataBoundItem;
+
+            prodAmounts.Add(nudAmount.Value);
+            
+            addedProducts.Add(selectedProduct);
+
+            dgvAdded.DataSource = new BindingList<Product>(addedProducts);
+
+            dgvAdded.Refresh();
+        }
     }
 }
-// C:/xampp/htdocs/Barroc-Intens-App/BarrocIntensApp/text.txt
